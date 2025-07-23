cb68fc0a4c6e1295acb216e329d8944ddd8deb0d
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-07-23 19:57:27 -0300

    Adicionando arquivos .gitignore e docker-compose.yml para configuração do ambiente

---
+++ src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ead.authuser.service.impl;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repository.UserRepository;
+import com.ead.authuser.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Object getUserById(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+    }
+
+}
